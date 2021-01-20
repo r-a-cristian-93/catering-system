f218d3d36ebcf4358a5c268fddc4332e39dd297e
Alex <r.a.cristian.93@gmail.com>
2021-01-21 01:22:00 +0200

    Make repo.save(newEntity) really save the entity in DB and return it

--- rest/src/main/java/com/catering/rest/Application.java
+++ rest/src/main/java/com/catering/rest/Application.java
@@ -3,9 +3,13 @@ package com.catering.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.catering.rest.db.repositories.CustomBaseRepository;
+
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomBaseRepository.class)
 public class Application {
 
 	public static void main(String[] args) {
---
+++ rest/src/main/java/com/catering/rest/db/repositories/CustomBaseRepository.java
@@ -0,0 +1,33 @@
+package com.catering.rest.db.repositories;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public class CustomBaseRepository<T, ID> extends SimpleJpaRepository<T, ID> {
+	private final EntityManager entityManager;
+	private final JpaEntityInformation<T, ?> entityInformation;
+
+	CustomBaseRepository(JpaEntityInformation<T, ?>  entityInformation, EntityManager entityManager) {
+		super(entityInformation, entityManager);
+		this.entityManager = entityManager;
+		this.entityInformation = entityInformation;
+	}
+
+	@Transactional
+	@Override
+	public <S extends T> S save(S entity) {
+		if (entityInformation.isNew(entity)) {
+			entityManager.persist(entity);
+			entityManager.refresh(entity);
+			return entity;
+		} else {
+			return entityManager.merge(entity);
+		}		
+	}
+}
