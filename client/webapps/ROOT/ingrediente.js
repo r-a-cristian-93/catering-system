0503c8e7a641b163de07ca5918e131f48d597be5
Alex <r.a.cristian.93@gmail.com>
2022-09-11 20:11:33 +0300

    Added unit selector for ingredients

--- client/webapps/ROOT/ingrediente.js
+++ client/webapps/ROOT/ingrediente.js
@@ -1,8 +1,8 @@
 /* ********** INGREDIENT **********/
 
-$(document).ready(function(){		
+$(document).ready(function(){
 	var args = {
-		page: 0, 
+		page: 0,
 		size: localStorage.PAGE_SIZE
 	}
 	ingredientBuildTableAll(args);
@@ -16,7 +16,7 @@ function getIngredients(args) {
 		xhrFields: { withCredentials: true },
 		dataType: 'json',
 		url: DEFAULTS.REST_URL + '/ingredients/allPageable',
-		data: { "page": args.page, "size": args.size }		
+		data: { "page": args.page, "size": args.size }
 	});
 }
 
@@ -39,7 +39,7 @@ function updateIngredient(ingredient) {
 		dataType: 'json',
 		contentType: "application/json",
 		data: JSON.stringify(ingredient)
-	});	
+	});
 }
 
 function deleteIngredient(id) {
@@ -60,7 +60,7 @@ function ingredientAdd() {
 	};
 	$.when(addIngredient(ingredient)).then(function(data){
 		$("table").append(newIngRow(data))
-	});	
+	});
 }
 
 function ingredientUpdate(id) {
@@ -84,16 +84,16 @@ function ingredientDelete(id) {
 function ingredientBuildTable(args) {
 	$.when(args.getFunction(args)).then(function(ingredients){
 		args.currentPage = ingredients.pageable.pageNumber;
-		args.totalPages = ingredients.totalPages;	
-		
+		args.totalPages = ingredients.totalPages;
+
 		var table = $("<table>")
 			.addClass('full')
 			.append(newHeader(["ID", "Denumire", "Pret [Lei]", "U/M"]));
-			
+
 		for(ing of ingredients.content) {
 			table.append(newIngRow(ing));
 		}
-		
+
 		$("#ing-table").html("")
 			.append(table)
 			.append(
@@ -102,8 +102,37 @@ function ingredientBuildTable(args) {
 					.attr({"onclick": "ingredientAdd()"})
 					.html("+ Adauga ingredient nou"))
 			.append(newPager(args));
-		});	
+		});
 }
+
+
+function newUnitSelectOption(ing_id, new_unit) {
+	return $("<a>").text(new_unit).on("click", function() {
+		ingredientUpdate(ing_id);
+	});
+}
+
+function newUnitSelector(ing_id, current_unit) {
+	var unitSelectorDiv =  $("<div>").addClass("select-list-container")
+		.append($("<div>").addClass("select-list-current").text(current_unit))
+		.on("click", function(e){
+			this.classList.add("dropdown");
+		})
+		.on("mouseleave", function(e){
+			this.classList.remove("dropdown");
+		});
+
+	var ddc = newDivDDC([]);
+	var unitsArray = JSON.parse(localStorage.getItem("UNITS"));
+	for (unit of unitsArray) {
+		console.log(unit);
+		ddc.append(newUnitSelectOption(ing_id, unit));
+	}
+
+	unitSelectorDiv.append(ddc);
+	return unitSelectorDiv;
+}
+
 
 function newIngRow(ing) {
 	var saveButton = $("<img>")
@@ -113,23 +142,25 @@ function newIngRow(ing) {
 		.addClass("active")
 		.attr({"src": "/img/delete.png"})
 		.attr({"onclick": "ingredientDelete("+ing.id+")"});
-	
+
+	var divUnit = newUnitSelector(ing.id, ing.unit.name);
+
 	return newRow([
-		ing.id, 
-		ing.name, 
-		ing.price, 
-		ing.unit.name,
+		ing.id,
+		ing.name,
+		ing.price,
+		divUnit,
 		saveButton,
 		deleteButton
-	], [0, 1, 1, 1, 0, 0])
+	], [0, 1, 1, 0, 0, 0])
 		.on("input", function() {
 			enableSaveIngredient(this.id)});
-}	
+}
 
 function enableSaveIngredient(id) {
 		$("#" + id + " td:eq(4) > img")
 		.attr({"class":"active"})
-		.attr({"onclick": "ingredientUpdate("+id+")"});	
+		.attr({"onclick": "ingredientUpdate("+id+")"});
 }
 
 function disableSaveIngredient(id) {
@@ -141,5 +172,5 @@ function disableSaveIngredient(id) {
 function ingredientBuildTableAll(args) {
 	args.buildFunction = ingredientBuildTableAll;
 	args.getFunction = getIngredients;
-	ingredientBuildTable(args);	
+	ingredientBuildTable(args);
 }
