a629644b59c2febfd64b541ecbad278dd85c956e
Alex <r.a.cristian.93@gmail.com>
2022-11-13 20:34:27 +0200

    Added service layer for ingredients

---
+++ rest/src/main/java/com/catering/rest/db/services/IngredientsService.java
@@ -0,0 +1,55 @@
+package com.catering.rest.db.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.catering.rest.db.models.IngredientModel;
+import com.catering.rest.db.models.UnitModel;
+import com.catering.rest.db.repositories.IngredientsRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class IngredientsService {
+    private final IngredientsRepository ingredientsRepo;
+
+	public Iterable<IngredientModel> getIngredients() {
+		return ingredientsRepo.findAll();
+	}
+
+	public IngredientModel addIngredient(IngredientModel ingredient) {
+		return ingredientsRepo.save(ingredient);
+	}
+
+	public IngredientModel getIngredient(Integer id) {
+		return ingredientsRepo.findById(id).get();
+	}
+
+	public void deleteIngredient(Integer id) {
+		ingredientsRepo.deleteById(id);
+	}
+
+	public IngredientModel updateIngredient(Integer id, IngredientModel ingredient) {
+		String name = ingredient.getName();
+		UnitModel unit = ingredient.getUnit();
+		ingredient = ingredientsRepo.findById(id).get();
+
+		if(name!=null) {
+			ingredient.setName(name);
+		}
+		if(unit!=null) {
+			ingredient.setUnit(unit);
+		}
+		return ingredientsRepo.save(ingredient);
+	}
+
+
+	// PAGEABLE
+
+	public Page<IngredientModel> getIngredientsPageable(Integer page, Integer size) {
+		return ingredientsRepo.findAll(PageRequest.of(page, size));
+	}
+
+}
