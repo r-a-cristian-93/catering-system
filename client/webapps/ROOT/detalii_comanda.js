2334229a4b089d3cca1aa18022365cd4bc7abb8c
Alex <r.a.cristian.93@gmail.com>
2022-11-26 14:08:20 +0200

    Added all order detail cards

--- client/webapps/ROOT/detalii_comanda.js
+++ client/webapps/ROOT/detalii_comanda.js
@@ -55,19 +55,32 @@ function newCards(order) {
 			.append($("<div>").addClass("stretch-bg profil")))
 		.append($("<div>").addClass("card-details")
 			.append($("<div>").addClass("card-title").html("Client"))
-			//.append($("<div>").addClass("card-text-small").html("&nbsp"))
 			.append($("<div>").addClass("card-text-big first-big").html(order.client.name))
 			.append($("<div>").addClass("card-text-medium").html(order.client.phone))
 		);
 
-	//var cardAddress = $("<div>").addClass(order.status.name.replace(/ /g, "-"));
-	//var cardDeadline = $("<div>").addClass(order.status.name.replace(/ /g, "-"));
+	var cardAddress = $("<div>").addClass("card").attr({"id": "card-address"})
+		.append($("<div>").addClass("card-icon")
+			.append($("<div>").addClass("stretch-bg " + order.status.name.replace(/ /g, "-"))))
+		.append($("<div>").addClass("card-details")
+			.append($("<div>").addClass("card-title").html("Adresa livrare"))
+			.append($("<div>").addClass("card-text-medium").html(order.deliveryAddress.value))
+		);
+
+	var cardDeadline = $("<div>").addClass("card").attr({"id": "card-deadline"})
+		.append($("<div>").addClass("card-icon")
+			.append($("<div>").addClass("stretch-bg " + order.status.name.replace(/ /g, "-"))))
+		.append($("<div>").addClass("card-details")
+			.append($("<div>").addClass("card-title").html("Termen livrare"))
+			.append($("<div>").addClass("card-text-medium").html("10.10.2020"))
+			.append($("<div>").addClass("card-text-big").html("12:00"))
+		);
 
 	var cards = $("<div>").attr({"id": "cards"})
 		.append(cardStatus)
 		.append(cardClient)
-		.append(cardStatus.clone())
-		.append(cardStatus.clone());
+		.append(cardAddress)
+		.append(cardDeadline);
 
 
 	return cards;
