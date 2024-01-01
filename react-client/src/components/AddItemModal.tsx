9050e88697c8742cf4f8f9a0bffa77ee4069480a
Alex <r.a.cristian.93@gmail.com>
2024-01-01 12:27:53 +0200

    use tabs

--- react-client/src/components/AddItemModal.tsx
+++ react-client/src/components/AddItemModal.tsx
@@ -7,60 +7,59 @@ import { useState } from "react";
 import { queryClient } from "../main";
 
 type AddItemModalProps = {
-    toogleModalCallback: () => void;
-    addSuccessfulCallback: (orderItem: OrderItem) => void;
-    orderId: number;
+	toogleModalCallback: () => void;
+	addSuccessfulCallback: (orderItem: OrderItem) => void;
+	orderId: number;
 }
 
 export default function AddItemModal(props: AddItemModalProps): JSX.Element
 {
-    useQuery<Recipe[]>({
-        queryKey: [ "recipes", props.orderId ],
-        queryFn: () => getRecipes(),
-        onSuccess: (recipes) =>
-        {
-            setRecipes(recipes);
-        }
-    });
+	useQuery<Recipe[]>({
+		queryKey: [ "recipes", props.orderId ],
+		queryFn: () => getRecipes(),
+		onSuccess: (recipes) =>
+		{
+			setRecipes(recipes);
+		}
+	});
 
-    const [ recipes, setRecipes ] = useState<Recipe[] | null>(
-        queryClient.getQueryData([ "recipes", props.orderId ]) as Recipe[]
-    )
+	const [ recipes, setRecipes ] = useState<Recipe[] | null>(
+		queryClient.getQueryData([ "recipes", props.orderId ]) as Recipe[]
+	)
 
-    return (
-        <div className="modal" id="edit-order-details-modal">
-            <div className="modal-container">
-                <div className="modal-box">
-                    <div className="modal-top">
-                        <h2 className="modal-title">Adauga articol</h2>
-                        <span className="modal-close no-print" onClick={props.toogleModalCallback}>×</span>
-                    </div>
-                    <div className="modal-content">
-                        <table className="full">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Reteta</th>
-                                    <th>Gramaj</th>
-                                    <th>Cost unitar</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    recipes?.map((recipe) =>
-                                    {
-                                        return <AddItem
-                                            key={recipe.id}
-                                            orderId={props.orderId}
-                                            recipe={recipe}
-                                            addSuccessfulCallback={props.addSuccessfulCallback} />
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="modal" id="edit-order-details-modal">
+			<div className="modal-container">
+				<div className="modal-box">
+					<div className="modal-top">
+						<h2 className="modal-title">Adauga articol</h2>
+						<span className="modal-close no-print" onClick={props.toogleModalCallback}>×</span>
+					</div>
+					<div className="modal-content">
+						<table className="full">
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Reteta</th>
+									<th>Gramaj</th>
+									<th>Cost unitar</th>
+								</tr>
+							</thead>
+							<tbody>
+								{
+									recipes?.map((recipe) =>
+										<AddItem
+											key={recipe.id}
+											orderId={props.orderId}
+											recipe={recipe}
+											addSuccessfulCallback={props.addSuccessfulCallback} />
+									)
+								}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
