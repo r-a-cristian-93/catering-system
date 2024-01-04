103089dfca0c3e5f98c1753848e5764309dc590f
Alex <r.a.cristian.93@gmail.com>
2024-01-04 13:17:20 +0200

    ui edit input icon

--- react-client/src/components/AddItem.tsx
+++ react-client/src/components/AddItem.tsx
@@ -5,61 +5,72 @@ import { Recipe } from "../models/Recipe/Recipe";
 import * as Formatter from "../utils/Formatting";
 
 type AddItemProps = {
-    orderId: number;
-    recipe: Recipe;
-    addSuccessfulCallback: (orderItem: OrderItem) => void
+	orderId: number;
+	recipe: Recipe;
+	addSuccessfulCallback: (orderItem: OrderItem) => void
 };
 
 export default function AddItem(props: AddItemProps): JSX.Element
 {
-    const [ orderItem, setOrderItem ] = useState<OrderItem>(
-        {
-            order: { id: props.orderId } as Order,
-            recipe: props.recipe,
-            servings: 0,
-        } as OrderItem
-    )
+	const [ orderItem, setOrderItem ] = useState<OrderItem>(
+		{
+			order: { id: props.orderId } as Order,
+			recipe: props.recipe,
+			servings: 0,
+		} as OrderItem
+	)
 
-    function handleAddItem(): void
-    {
-        if (orderItem.servings)
-        {
-            void addOrderItem(orderItem).then((item) =>
-            {
-                props.addSuccessfulCallback(item);
-            });
-        }
-    }
+	function handleAddItem(): void
+	{
+		if (orderItem.servings)
+		{
+			void addOrderItem(orderItem).then((item) =>
+			{
+				props.addSuccessfulCallback(item);
+			});
+		}
+	}
 
-    function handleChange(event: ChangeEvent<HTMLInputElement>): void
-    {
-        const { name, value } = event.target;
+	function handleChange(event: ChangeEvent<HTMLInputElement>): void
+	{
+		const { name, value } = event.target;
 
-        if (name === "servings")
-            (Number(value) >= 0) && setOrderItem((prev) => ({
-                ...prev,
-                [ name ]: Number(value)
-            }));
-    }
+		if (name === "servings")
+			(Number(value) >= 0) && setOrderItem((prev) => ({
+				...prev,
+				[ name ]: Number(value)
+			}));
+	}
 
-    return (
-        <tr id="2">
-            <td>{orderItem.recipe.id}</td>
-            <td>{orderItem.recipe.name}</td>
-            <td>{orderItem.recipe.quantity + " " + orderItem.recipe.unit.name}</td>
-            <td>{Formatter.formatCurrency(orderItem.recipe.ingCost)}</td>
-            <td><input name="servings" value={orderItem.servings || ""} type="number" size={4} onChange={handleChange}></input></td>
-            <td width={100}>
-                {
-                    orderItem.servings &&
-                    Formatter.formatCurrency(orderItem.recipe.ingCost * orderItem.servings) || ""
-                }
-            </td>
-            <td><img
-                src="/img/add.png"
-                onClick={handleAddItem}
-                className={orderItem.servings ? "" : "inactive"}
-            /></td>
-        </tr>
-    );
+	return (
+		<tr id="2">
+			<td>{orderItem.recipe.id}</td>
+			<td>{orderItem.recipe.name}</td>
+			<td>{orderItem.recipe.quantity + " " + orderItem.recipe.unit.name}</td>
+			<td>{Formatter.formatCurrency(orderItem.recipe.ingCost)}</td>
+			<td>
+				<label className="editable">
+					<input
+						type="number" size={4}
+						name="servings"
+						value={orderItem.servings || ""}
+						onChange={handleChange}
+						placeholder="0"
+						min="0"
+					/>
+				</label>
+			</td>
+			<td width={100}>
+				{
+					orderItem.servings &&
+					Formatter.formatCurrency(orderItem.recipe.ingCost * orderItem.servings) || ""
+				}
+			</td>
+			<td><img
+				src="/img/add.png"
+				onClick={handleAddItem}
+				className={orderItem.servings ? "" : "inactive"}
+			/></td>
+		</tr>
+	);
 }
--- react-client/src/components/OrderItemComponent.tsx
+++ react-client/src/components/OrderItemComponent.tsx
@@ -3,57 +3,67 @@ import * as Formatter from "../utils/Formatting";
 import { ChangeEvent } from "react";
 
 type OrderItemProps = {
-    orderItem: OrderItem;
-    changeCallback: (orderItem: OrderItem) => void;
-    deleteCallback: (orderItem: OrderItem) => void;
+	orderItem: OrderItem;
+	changeCallback: (orderItem: OrderItem) => void;
+	deleteCallback: (orderItem: OrderItem) => void;
 };
 
 export default function OrderItemComponent(props: OrderItemProps): JSX.Element
 {
-    const orderItem = props.orderItem;
-    const { changeCallback, deleteCallback } = props;
+	const orderItem = props.orderItem;
+	const { changeCallback, deleteCallback } = props;
 
-    const costTotal = orderItem.recipe.ingCost * orderItem.servings;
+	const costTotal = orderItem.recipe.ingCost * orderItem.servings;
 
-    function handleChange(event: ChangeEvent<HTMLInputElement>): void
-    {
-        const { name, value } = event.target;
+	function handleChange(event: ChangeEvent<HTMLInputElement>): void
+	{
+		const { name, value } = event.target;
 
-        if (name === "servings")
-        {
-            const newItem = {
-                ...orderItem,
-                [ name ]: Number(value),
-            };
+		if (name === "servings")
+		{
+			const newItem = {
+				...orderItem,
+				[ name ]: Number(value),
+			};
 
-            changeCallback(newItem);
-        }
-    }
+			changeCallback(newItem);
+		}
+	}
 
-    function handleOnBlur(): void
-    {
-        void updateOrderItem(orderItem);
-    }
+	function handleOnBlur(): void
+	{
+		void updateOrderItem(orderItem);
+	}
 
-    function handleDelete(): void
-    {
-        void deleteOrderItem(orderItem).then((isItemDeleted) =>
-        {
-            if (isItemDeleted) deleteCallback(orderItem);
-        });
-    }
+	function handleDelete(): void
+	{
+		void deleteOrderItem(orderItem).then((isItemDeleted) =>
+		{
+			if (isItemDeleted) deleteCallback(orderItem);
+		});
+	}
 
-    return (
-        <tr id="det_8" className="font-size-120">
-            <td>{orderItem.recipe.name}</td>
-            <td>
-                <input name="servings" type="number" value={orderItem.servings} onChange={handleChange} onBlur={handleOnBlur} />
-            </td>
-            <td>{Formatter.formatCurrency(orderItem.recipe.ingCost)}</td>
-            <td>{Formatter.formatCurrency(costTotal)}</td>
-            <td>
-                <img className="active" src="/img/delete.png" onClick={handleDelete} />
-            </td>
-        </tr>
-    );
+	return (
+		<tr id="det_8" className="font-size-120">
+			<td>{orderItem.recipe.name}</td>
+			<td>
+				<label className="editable">
+					<input
+						name="servings"
+						type="number"
+						value={orderItem.servings}
+						onChange={handleChange}
+						onBlur={handleOnBlur}
+						placeholder="0"
+						min="0"
+					/>
+				</label>
+			</td>
+			<td>{Formatter.formatCurrency(orderItem.recipe.ingCost)}</td>
+			<td>{Formatter.formatCurrency(costTotal)}</td>
+			<td>
+				<img className="active" src="/img/delete.png" onClick={handleDelete} />
+			</td>
+		</tr>
+	);
 }
