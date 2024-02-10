76d1398de0416f913a5156bf899666ce9383db00
Alex <r.a.cristian.93@gmail.com>
2024-02-10 11:36:09 +0200

    get all report data

--- react-client/src/controllers/OrderReportsController.tsx
+++ react-client/src/controllers/OrderReportsController.tsx
@@ -5,7 +5,27 @@ export type ReportByDate = {
     count: number;
 }
 
-export async function getReport(startDateMilis: number, endDateMilis: number): Promise<ReportByDate[]>
+export async function getReportOfPlacementDate(startDateMilis: number, endDateMilis: number): Promise<ReportByDate[]>
+{
+    return getReport("reportOfPlacementDate", startDateMilis, endDateMilis);
+}
+
+export async function getReportOfDueDate(startDateMilis: number, endDateMilis: number): Promise<ReportByDate[]>
+{
+    return getReport("reportOfDueDate", startDateMilis, endDateMilis);
+}
+
+export async function getReportOfCancelDate(startDateMilis: number, endDateMilis: number): Promise<ReportByDate[]>
+{
+    return getReport("reportOfCancelDate", startDateMilis, endDateMilis);
+}
+
+export async function getReportOfShippingDate(startDateMilis: number, endDateMilis: number): Promise<ReportByDate[]>
+{
+    return getReport("reportOfShippingDate", startDateMilis, endDateMilis);
+}
+
+async function getReport(path: string, startDateMilis: number, endDateMilis: number): Promise<ReportByDate[]>
 {
     const queryParameters = new URLSearchParams(
 		{
@@ -14,7 +34,7 @@ export async function getReport(startDateMilis: number, endDateMilis: number): P
 		}
 	);
 
-	const url = VITE_API_URL + "/orders/reportOfPlacementDate?" + queryParameters.toString();
+	const url = VITE_API_URL + "/orders/"+ path +"?" + queryParameters.toString();
 
 	const response = await fetch(url, {
 		method: "GET",
--- react-client/src/pages/HomePage.tsx
+++ react-client/src/pages/HomePage.tsx
@@ -1,16 +1,16 @@
 import { LineChart } from "@mui/x-charts";
-import { getReport } from "../controllers/OrderReportsController";
+import { ReportByDate, getReportOfCancelDate, getReportOfDueDate, getReportOfPlacementDate, getReportOfShippingDate } from "../controllers/OrderReportsController";
 import { useEffect, useState } from "react";
 import * as Formatter from "../utils/Formatting";
 
-export default function HomePage(): JSX.Element
-{
-	const startDateMilis: number =  Date.parse("2020-01-01");
-	const endDateMilis: number = Date.parse("2050-02-01");
-
-	useEffect(()=>
+function setReportData(
+	startDateMillis: number,
+	endDateMillis: number,
+	fetchFunction: (start: number, end: number) => Promise<ReportByDate[]>,
+	setFunction: (value: React.SetStateAction<Map<string, number>>) => void
+): void
 	{
-		void getReport(startDateMilis, endDateMilis).then((report) =>
+		void fetchFunction(startDateMillis, endDateMillis).then((report) =>
 		{
 			const reportMap: Map<string, number> = new Map(report.map((entry) =>
 			{
@@ -20,11 +20,27 @@ export default function HomePage(): JSX.Element
 				]
 			}));
 
-			setReportPlacementDate(reportMap);
+			setFunction(reportMap);
 		});
+	}
+
+export default function HomePage(): JSX.Element
+{
+	const startDateMilis: number =  Date.parse("2020-01-01");
+	const endDateMilis: number = Date.parse("2050-02-01");
+
+	useEffect(()=>
+	{
+		setReportData(startDateMilis, endDateMilis, getReportOfPlacementDate, setReportPlacementDate);
+		setReportData(startDateMilis, endDateMilis, getReportOfDueDate, setReportDueDate);
+		setReportData(startDateMilis, endDateMilis, getReportOfCancelDate, setReportCancelDate);
+		setReportData(startDateMilis, endDateMilis, getReportOfShippingDate, setReportShippingDate);
 	}, [] );
 
 	const [reportPlacementDate, setReportPlacementDate] = useState<Map<string, number>>(new Map<string, number>([]));
+	const [reportDueDate, setReportDueDate] = useState<Map<string, number>>(new Map<string, number>([]));
+	const [reportCancelDate, setReportCancelDate] = useState<Map<string, number>>(new Map<string, number>([]));
+	const [reportShippingDate, setReportShippingDate] = useState<Map<string, number>>(new Map<string, number>([]));
 
 	return (<>
 		<div className="box-content" id="order-table">
@@ -33,10 +49,10 @@ export default function HomePage(): JSX.Element
 		<LineChart
 			xAxis={[
 				{
-				id: 'barCategories',
-				data: Array.from(reportPlacementDate.keys()).slice(),
-				scaleType: 'point',
-				},
+					id: 'barCategories',
+					data: Array.from(reportPlacementDate.keys()).slice(),
+					scaleType: 'point',
+				}
 			]}
 			yAxis={[
 				{
@@ -48,7 +64,7 @@ export default function HomePage(): JSX.Element
 					id: "placement",
 					data: Array.from(reportPlacementDate.values()),
 					label: "Order Placement",
-				},
+				}
 			]}
 			width={500}
 			height={300}
