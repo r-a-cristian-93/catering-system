9c3d8ab9fbdba193fa4034633fc44cbf13ad6f51
Alex <r.a.cristian.93@gmail.com>
2024-03-08 20:37:26 +0200

    axios = AddressController

--- react-client/src/controllers/AddressControllere.tsx
+++ react-client/src/controllers/AddressControllere.tsx
@@ -1,4 +1,5 @@
 import { ClientAddress } from "../models/Order";
+import axios from "axios";
 
 const { VITE_API_URL } = import.meta.env;
 
@@ -6,38 +7,16 @@ export async function getAddresses(clientId: number): Promise<ClientAddress[]>
 {
     const url = VITE_API_URL + "/clients/" + clientId + "/addresses";
 
-    const response = await fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        credentials: "include",
-    });
+    const response = await axios.get<ClientAddress[]>(url, { withCredentials: true });
 
-    return response.json();
+    return response.data;
 }
 
 export async function addAddress(address: ClientAddress): Promise<ClientAddress>
 {
     const url = VITE_API_URL + "/addresses";
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: "include",
-        body: JSON.stringify(address),
-    });
+    const response = await axios.post<ClientAddress>(url, address, { withCredentials: true });
 
-    const addressPromise: Promise<ClientAddress> = response.json().then((json) =>
-    {
-        const createdAddres: ClientAddress = {} as ClientAddress;
-
-        Object.assign(createdAddres, json);
-
-        return createdAddres;
-    });
-
-    return addressPromise;
+    return response.data;
 }
