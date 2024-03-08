ca87261ef87f0fa04b851f0df4a6c658ec65889c
Alex <r.a.cristian.93@gmail.com>
2024-03-08 20:37:26 +0200

    axios - RecipeController

--- react-client/src/controllers/RecipeController.tsx
+++ react-client/src/controllers/RecipeController.tsx
@@ -1,51 +1,21 @@
 import { Recipe } from "../models/Recipe";
+import axios from "axios";
 const { VITE_API_URL } = import.meta.env;
 
 export async function getRecipe(recipeId: number): Promise<Recipe>
 {
 	const url = VITE_API_URL + "/recipes/" + recipeId.toString();
 
-	const response = await fetch(url, {
-		method: "GET",
-		credentials: "include",
-		headers: {
-			"Content-Type" : "application/json"
-		}
-	});
+	const response = await axios.get<Recipe>(url, { withCredentials: true });
 
-	const recipePromise: Promise<Recipe> = response.json().then((json) =>
-	{
-		const recipe: Recipe = {} as Recipe;
-
-		Object.assign(recipe, json);
-
-		return recipe;
-	})
-
-	return recipePromise;
+	return response.data;
 }
 
 export async function updateRecipe(recipe: Recipe): Promise<Recipe>
 {
 	const url = VITE_API_URL + "/recipes/" + recipe.id;
 
-	const response = await fetch(url, {
-		method: "PUT",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(recipe),
-	});
+	const response = await axios.put<Recipe>(url, recipe, { withCredentials: true });
 
-	const recipePromise: Promise<Recipe> = response.json().then((json) =>
-	{
-		const recipe: Recipe = {} as Recipe;
-
-		Object.assign(recipe, json);
-
-		return recipe;
-	})
-
-	return recipePromise;
+	return response.data;
 }
