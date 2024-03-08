6668d711554ad425f668d93180003d426c27f263
Alex <r.a.cristian.93@gmail.com>
2024-03-08 20:37:26 +0200

    axios - OrderController

--- react-client/src/controllers/OrderController.tsx
+++ react-client/src/controllers/OrderController.tsx
@@ -1,4 +1,5 @@
 import { Order } from "../models/Order";
+import axios from "axios";
 
 const { VITE_API_URL } = import.meta.env;
 
@@ -6,69 +7,25 @@ export async function getOrder(orderId: number): Promise<Order>
 {
 	const url = VITE_API_URL + "/orders/" + orderId.toString();
 
-	const response = await fetch(url, {
-		method: "GET",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/json",
-		}
-	});
+	const response = await axios.get<Order>(url, {withCredentials: true});
 
-	const orderPromise: Promise<Order> = response.json().then((json) =>
-	{
-		const order: Order = {} as Order;
-		Object.assign(order, json);
-
-		return order;
-	});
-
-	return orderPromise;
+	return response.data;
 }
 
 export async function setNextOrderState(orderId: number): Promise<Order>
 {
 	const url = VITE_API_URL + "/orders/" + orderId + "/nextstep";
 
-	const response = await fetch(url, {
-		method: "PUT",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+	const response = await axios.put<Order>(url, null, { withCredentials: true });
 
-	const orderPromise: Promise<Order> = response.json().then((json) =>
-	{
-		const order: Order = {} as Order;
-		Object.assign(order, json);
-
-		return order;
-	});
-
-	return orderPromise;
+	return response.data;
 }
 
 export async function updateOrder(order: Order): Promise<Order>
 {
 	const url = VITE_API_URL + "/orders/" + order.id;
 
-	const response = await fetch(url, {
-		method: "PUT",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(order),
-	});
+	const response = await axios.put<Order>(url, order, { withCredentials: true });
 
-	const orderPromise: Promise<Order> = response.json().then((json) =>
-	{
-		const order: Order = {} as Order;
-
-		Object.assign(order, json);
-
-		return order;
-	})
-
-	return orderPromise;
+	return response.data;
 }
