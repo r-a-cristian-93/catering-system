6850e6bf31500a5b9ee7f95052b1b2a15378e31f
Alex <r.a.cristian.93@gmail.com>
2024-03-10 12:00:51 +0200

    css module TableList

--- react-client/src/components/generic/TableList/TableList.tsx
+++ react-client/src/components/generic/TableList/TableList.tsx
@@ -1,7 +1,9 @@
+import css from "./TableList.module.css"
+
 export default function TableList(props: React.HTMLProps<HTMLTableElement>): JSX.Element
 {
 	return (
-		<table {...props} className={props.className + " table-list"}>
+		<table {...props} className={props.className + " " + css.table_list}>
 			{props.children}
 		</table>
 	);
