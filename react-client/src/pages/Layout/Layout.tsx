2cc541dc5647c597722f9bf489abf7500b885379
Alex <r.a.cristian.93@gmail.com>
2024-03-10 10:40:42 +0200

    move Auth

--- react-client/src/pages/Layout/Layout.tsx
+++ react-client/src/pages/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../../components/generic/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import LoginPage from "../LoginPage/LoginPage";
-import isUserAuthenticated from "../../components/Auth";
+import isUserAuthenticated from "../../utils/Auth";
 import css from "./Layout.module.css"
 
 export default function Layout(): JSX.Element
