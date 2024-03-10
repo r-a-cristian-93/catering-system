46dbc63aac8cf827e3d802ff0586e9a5767d5473
Alex <r.a.cristian.93@gmail.com>
2024-03-10 10:39:44 +0200

    css module Layout

--- react-client/src/pages/Layout/Layout.tsx
+++ react-client/src/pages/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/generic/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import LoginPage from "../LoginPage/LoginPage";
 import isUserAuthenticated from "../../components/Auth";
+import css from "./Layout.module.css"
 
 export default function Layout(): JSX.Element
 {
@@ -10,7 +11,7 @@ export default function Layout(): JSX.Element
 			{
 				isUserAuthenticated() && <Navbar />
 			}
-			<div className="content">
+			<div className={css.content}>
 				{
 					isUserAuthenticated() ? <Outlet /> : <LoginPage />
 				}
