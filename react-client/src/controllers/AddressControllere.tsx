f48347faf677daf47936c9512b2deec4470fe840
Alex <r.a.cristian.93@gmail.com>
2024-03-24 16:15:00 +0200

    remove unused code

--- react-client/src/controllers/AddressControllere.tsx
+++ react-client/src/controllers/AddressControllere.tsx
@@ -3,15 +3,6 @@ import axios from "axios";
 
 const { VITE_API_URL } = import.meta.env;
 
-// export async function getAddresses(clientId: number): Promise<ClientAddress[]>
-// {
-//     const url = VITE_API_URL + "/clients/" + clientId + "/addresses";
-
-//     const response = await axios.get<ClientAddress[]>(url, { withCredentials: true });
-
-//     return response.data;
-// }
-
 export async function addAddress(address: ClientAddress): Promise<ClientAddress>
 {
     const url = VITE_API_URL + "/address";
